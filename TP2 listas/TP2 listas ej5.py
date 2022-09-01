5edb59d601206e6fdf19aa82e5589d379e296795
daniblanco1998 <danieloropeza98sc@gmail.com>
2022-09-01 15:56:24 -0300

    Cambios 1 de sep

--- TP2 listas/TP2 listas ej5.py
+++ TP2 listas/TP2 listas ej5.py
@@ -1,23 +1,24 @@
 #Escribir una funcion q reciba una lista como parametro y devuelva True si la lista esta ordenadda en forma ascendente
 # o False en caso contrario.
 
+import random
+
 def esOrdenada(lista):
     res = True
     for i in range(len(lista)-1):
-        if(lista[i]>lista[i+1]):
+        if(lista[i]>=lista[i+1]):
             res = False
             break
     return res
 
-order_des=["c", "b", "a"]
-order_as=["a", "b", "c"]
 
-respuesta = esOrdenada(order_as)
-print(order_as)
-print(respuesta, "\n")
+def crearLista():
+    lista = []
+    for i in range(5):
+        lista.append(random.randint(0, 20))
+    return lista
 
-respuesta = esOrdenada(order_des)
-print(order_des)
+myList = crearLista()
+respuesta = esOrdenada(myList)
+print(myList)
 print(respuesta)
-
-
